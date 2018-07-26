ad4c7077a04c4092023dbd3454ee09f85444f783
Holden Altaffer <haltaffer@gmail.com>
2018-07-26 00:30:39 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,9 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "studentss", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
